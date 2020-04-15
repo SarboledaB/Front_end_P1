75d8d33ceec743a25e3a7b9a528b19cf17c097b5
Sebastian Arboleda <sarboledab@eafit.edu.co>
2020-04-15 16:53:04 -0500

    Se modifica el gitignore y se crea el proyecto

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/sarboledab/Universidad/2020-1/ProyectoIntegrador/Front-end/Front_end_P1/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
